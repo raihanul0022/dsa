b435b3cdce67b6fc87244fdb52d9c19ea9a0d756
raihanul0022 <islamraihan002@gmail.com>
2026-07-16 23:32:51 +0600

    minor change in queue implementation

--- home work/queue.c
+++ home work/queue.c
@@ -65,18 +65,25 @@ int main()
     enqueue(&q, 12);
     enqueue(&q, 15);
     enqueue(&q, 1);
+    enqueue(&q, 10);
+    enqueue(&q, 120);
+
     if (isEmpty(&q))
     {
         printf("\nThe queue is empty.");
     }
+
     if (isFull(&q))
     {
         printf("\nThe queue is full.");
     }
-    printf("\nDequeuing item %d",dequeue(&q));
-    printf("\nDequeuing item %d",dequeue(&q));
-    printf("\nDequeuing item %d",dequeue(&q));
 
+    printf("\nDequeuing item %d", dequeue(&q));
+    printf("\nDequeuing item %d", dequeue(&q));
+    printf("\nDequeuing item %d", dequeue(&q));
+    printf("\nDequeuing item %d", dequeue(&q));
+    printf("\nDequeuing item %d", dequeue(&q));
+    
     if (isEmpty(&q))
     {
         printf("\nThe queue is empty.");
