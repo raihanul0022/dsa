266ab44888e59711b73383cca6b30b1938ef7482
raihanul0022 <islamraihan002@gmail.com>
2026-07-16 23:30:41 +0600

    queue implimentation code added

---
+++ home work/queue.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queue
+{
+    int size;
+    int front;
+    int rear;
+    int *arr;
+};
+int isFull(struct queue *q)
+{
+    if (q->rear == q->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void enqueue(struct queue *q, int value)
+{
+    if (isFull(q))
+    {
+        printf("This queue is full.");
+    }
+    else
+    {
+        q->rear++;
+        q->arr[q->rear] = value;
+    }
+}
+
+int isEmpty(struct queue *q)
+{
+    if (q->rear == q->front)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int dequeue(struct queue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("The array is empty.");
+    }
+    else
+    {
+        q->front++;
+        return q->arr[q->front];
+    }
+}
+int main()
+{
+    struct queue q;
+    q.size = 5;
+    q.front = q.rear = -1;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    enqueue(&q, 12);
+    enqueue(&q, 15);
+    enqueue(&q, 1);
+    if (isEmpty(&q))
+    {
+        printf("\nThe queue is empty.");
+    }
+    if (isFull(&q))
+    {
+        printf("\nThe queue is full.");
+    }
+    printf("\nDequeuing item %d",dequeue(&q));
+    printf("\nDequeuing item %d",dequeue(&q));
+    printf("\nDequeuing item %d",dequeue(&q));
+
+    if (isEmpty(&q))
+    {
+        printf("\nThe queue is empty.");
+    }
+    return 0;
+}
