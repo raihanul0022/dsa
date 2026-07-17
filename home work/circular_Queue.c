973f121f16077c6efdaedc0d18c5960b2591b534
raihanul0022 <islamraihan002@gmail.com>
2026-07-17 23:48:51 +0600

    circular queue

--- home work/circular_Queue.c
+++ home work/circular_Queue.c
@@ -8,18 +8,10 @@ struct circular_queue
     int rear;
     int count;
     int *arr;
-
 };
 int isFull(struct circular_queue *q)
 {
-    if ((q->size-1==q->count))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    return q->size == q->count;
 }
 void enqueue(struct circular_queue *q, int value)
 {
@@ -30,22 +22,15 @@ void enqueue(struct circular_queue *q, int value)
     else
     {
         q->arr[q->rear] = value;
-        q->rear=(q->rear+1)%q->size;
+        q->rear = (q->rear + 1) % q->size;
         q->count++;
-        printf("\nEnqueued item: %d",value);
+        printf("\nEnqueued item: %d", value);
     }
 }
 
 int isEmpty(struct circular_queue *q)
 {
-    if (q->count == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    return q->count == 0;
 }
 int dequeue(struct circular_queue *q)
 {
@@ -56,7 +41,7 @@ int dequeue(struct circular_queue *q)
     else
     {
         int value = q->arr[q->front];
-        q->front=(q->front+1)%q->size;
+        q->front = (q->front + 1) % q->size;
         q->count--;
         return value;
     }
@@ -65,7 +50,7 @@ int dequeue(struct circular_queue *q)
 int main()
 {
     struct circular_queue q;
-    q.size = 6; 
+    q.size = 5;
     q.front = q.rear = q.count = 0;
     q.arr = (int *)malloc(q.size * sizeof(int));
 
@@ -74,7 +59,6 @@ int main()
     enqueue(&q, 1);
     enqueue(&q, 10);
     enqueue(&q, 50);
-
 
     if (isEmpty(&q))
     {
@@ -91,8 +75,11 @@ int main()
     printf("\nDequeuing item %d", dequeue(&q));
     printf("\nDequeuing item %d", dequeue(&q));
     printf("\nDequeuing item %d", dequeue(&q));
-
-
+    
+    if (isEmpty(&q))
+    {
+        printf("\nThe queue is empty.");
+    }
     enqueue(&q, 1);
     enqueue(&q, 10);
     enqueue(&q, 120);
